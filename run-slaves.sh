a8f3187b37e404254a94846a8b85be9f9458d22d
David Pemán Ruiz <david.peman.ruiz@everis.com>
2019-02-12 10:25:55 +0100

    add run a lot of jenkins slaves script

---
+++ run-slaves.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+END=$1
+
+for i in $(seq 1 $END)
+do 
+    git checkout -b feature-$i
+    echo "# Feature-$i requirements" > README.md
+    git commit -am "Updated feature-$i README.md"
+    git push origin feature-$i
+done
+
+git checkout master
